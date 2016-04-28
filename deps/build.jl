adacb548049a5f54bf556fdc07af051a3a53bdb3
Changhyun Kwon <chkwon@gmail.com>
2016-04-28 10:02:15 -0400

    linux build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,14 @@ using BinDeps
 dl_dir = joinpath(dirname(dirname(@__FILE__)), "deps", "downloads")
 deps_dir = joinpath(dirname(dirname(@__FILE__)), "deps")
 lib_dir = joinpath(deps_dir, "usr", "lib")
+src_dir = joinpath(deps_dir, "src")
+
+bit = "64"
+if Int == Int32
+  bit = "32"
+elseif Int ==Int64
+  bit = "64"
+end
 
 # # Check the dependency of libgfortran (should be available from the Julia installation)
 # libgfortran = library_dependency("libgfortran", aliases=["libgfortran", "libgfortran.3"])
@@ -14,11 +22,17 @@ lib_dir = joinpath(deps_dir, "usr", "lib")
 # end
 
 
+
+
 # The main dependency
 libpath47julia = library_dependency("libpath47julia")
 libpath47_dylib = joinpath(deps_dir, "pathlib-master", "lib", "osx", "libpath47.dylib")
 libpath47julia_dylib = joinpath(deps_dir, "PathJulia-0.0.2", "lib", "osx", "libpath47julia.dylib")
 libgfortran_dylib = joinpath(deps_dir, "PathJulia-0.0.2", "lib", "osx", "libgfortran.3.dylib")
+
+libpath47_so64 = joinpath(src_dir, "pathlib-master", "lib", "linux$bit", "libpath47.so")
+libpath47julia_so64 = joinpath(src_dir, "PathJulia-master", "lib", "linux$bit", "libpath47julia.so")
+
 
 provides(BuildProcess,
     (@build_steps begin
@@ -27,37 +41,70 @@ provides(BuildProcess,
             FileDownloader("https://github.com/ampl/pathlib/archive/master.zip",
                             joinpath(deps_dir, "downloads", "pathlib.zip"))
             FileUnpacker(joinpath(deps_dir, "downloads", "pathlib.zip"), deps_dir, libpath47_dylib)
-            `cp $libpath47_dylib $lib_dir`
+            `cp -i $libpath47_dylib $lib_dir`
         end
         @build_steps begin
             FileDownloader("https://github.com/chkwon/PathJulia/archive/0.0.2.tar.gz",
                             joinpath(deps_dir, "downloads", "pathjulia.tar.gz"))
             FileUnpacker(joinpath(deps_dir, "downloads", "pathjulia.tar.gz"), deps_dir, libpath47julia_dylib)
-            `cp $libpath47julia_dylib $lib_dir`
-            `cp $libgfortran_dylib $lib_dir`
+            `cp -i $libpath47julia_dylib $lib_dir`
+            `cp -i $libgfortran_dylib $lib_dir`
         end
     end), libpath47julia, os = :Darwin)
 
+provides(BuildProcess,
+    (@build_steps begin
+        CreateDirectory(lib_dir, true)
+        CreateDirectory(src_dir, true)
+        @build_steps begin
+            ChangeDirectory(src_dir)
+            FileDownloader("https://github.com/ampl/pathlib/archive/master.zip",
+                            joinpath(dl_dir, "pathlib.zip"))
+            FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_so64)
+            # `cp -i $libpath47_so64 $lib_dir`
+        end
+        @build_steps begin
+            FileDownloader("https://github.com/chkwon/PathJulia/archive/master.zip",
+                            joinpath(dl_dir, "PathJulia.zip"))
+            FileUnpacker(joinpath(dl_dir, "PathJulia.zip"), src_dir, libpath47julia_so64)
+            # `cp -i $libpath47julia_so64 $lib_dir`
+        end
+        @build_steps begin
+            ChangeDirectory(src_dir)
+            `rm -rf pathlib`
+            `rm -rf PathJulia`
+            `mv pathlib-master pathlib`
+            `mv PathJulia-master PathJulia`
+        end
+        @build_steps begin
+            ChangeDirectory(joinpath(src_dir, "pathlib", "lib"))
+            `cp -i linux$bit/libpath47.so ../../../usr/lib/`
+        end
+        @build_steps begin
+            ChangeDirectory(joinpath(src_dir, "PathJulia", "src"))
+            `make linux$bit`
+            `cp -i ../lib/linux$bit/libpath47julia.so ../../../usr/lib/`
+        end
+    end), libpath47julia, os = :Linux)
 
 
-try
-    @BinDeps.install Dict(:libpath47julia => :libpath47julia)
+@BinDeps.install Dict(:libpath47julia => :libpath47julia)
 
 
 
 
-catch e
-    info("===================================================================")
-    info(" When the package installation fails, add the following directory  ")
-    info("    $lib_dir                       ")
-    info(" to 'DYLD_LIBRARY_PATH' in your '.bash_profile' by                 ")
-    info("    export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:\"$lib_dir\"")
-    info(" then close the terminal, reopen, and build the package again:     ")
-    info("    julia> Pkg.build(\"PATHSolver\")            ")
-    info(" Read https://github.com/chkwon/PATHSolver.jl for further information.")
-    info("===================================================================")
-    throw(e)
-end
+# catch e
+#     info("===================================================================")
+#     info(" When the package installation fails, add the following directory  ")
+#     info("    $lib_dir                       ")
+#     info(" to 'DYLD_LIBRARY_PATH' in your '.bash_profile' by                 ")
+#     info("    export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:\"$lib_dir\"")
+#     info(" then close the terminal, reopen, and build the package again:     ")
+#     info("    julia> Pkg.build(\"PATHSolver\")            ")
+#     info(" Read https://github.com/chkwon/PATHSolver.jl for further information.")
+#     info("===================================================================")
+#     throw(e)
+# end
 
 
 
