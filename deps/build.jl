6d9aa5bd453dee0e7c556d224af8c4f44b72df30
Changhyun Kwon <chkwon@usf.edu>
2016-04-28 11:41:16 -0400

    build updae

--- deps/build.jl
+++ deps/build.jl
@@ -13,18 +13,20 @@ elseif Int ==Int64
   bit = "64"
 end
 
-# libgfortran_path = filter(x -> endswith(x, "libgfortran.3.dylib"), Libdl.dllist())[1]
+
+gfortran_osx = filter(x -> endswith(x, "libgfortran.3.dylib"), Libdl.dllist())
+libgfortran_path = isempty(gfortran_osx) ? "" : gfortran_osx[1]
 
 
 # Verions of libraries
 pathlib_v = "a11966f36875748820583e41455800470c971171"
-pathjulia_v = "0.0.4"
+pathjulia_v = "master"
 
 # The main dependency
 libpath47julia = library_dependency("libpath47julia")
-libpath47_dylib = joinpath(deps_dir, "pathlib-$pathlib_v", "lib", "osx", "libpath47.dylib")
-libpath47julia_dylib = joinpath(deps_dir, "PathJulia-$pathjulia_v", "lib", "osx", "libpath47julia.dylib")
 
+libpath47_dylib = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "osx", "libpath47.dylib")
+libpath47julia_dylib = joinpath(src_dir, "PathJulia-$pathjulia_v", "lib", "osx", "libpath47julia.dylib")
 
 libpath47_so64 = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "linux$bit", "libpath47.so")
 path47julia_c = joinpath(src_dir, "PathJulia-$pathjulia_v", "src", "pathjulia.c")
@@ -34,19 +36,27 @@ provides(BuildProcess,
     (@build_steps begin
         CreateDirectory(lib_dir, true)
         @build_steps begin
-            FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip", joinpath(deps_dir, "downloads", "pathlib.zip"))
-            FileUnpacker(joinpath(deps_dir, "downloads", "pathlib.zip"), deps_dir, libpath47_dylib)
-            `cp -i $libpath47_dylib $lib_dir`
+            FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip", joinpath(dl_dir, "pathlib.zip"))
+            FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_dylib)
+            `cp -f $libpath47_dylib $lib_dir`
         end
         @build_steps begin
-            FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.tar.gz", joinpath(deps_dir, "downloads", "pathjulia.tar.gz"))
-            FileUnpacker(joinpath(deps_dir, "downloads", "pathjulia.tar.gz"), deps_dir, libpath47julia_dylib)
-            `cp $libpath47julia_dylib $lib_dir`
+            FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.zip", joinpath(dl_dir, "PathJulia.zip"))
+            FileUnpacker(joinpath(dl_dir, "PathJulia.zip"), src_dir, libpath47julia_dylib)
+            `cp -f $libpath47julia_dylib $lib_dir`
             @osx_only `install_name_tool -change /usr/local/lib/libgfortran.3.dylib @rpath/libgfortran.3.dylib $lib_dir/libpath47.dylib`
             @osx_only `install_name_tool -change /usr/local/lib/libgcc_s.1.dylib @rpath/libgcc_s.1.dylib $lib_dir/libpath47.dylib`
             @osx_only `install_name_tool -add_rpath $(dirname(libgfortran_path)) $lib_dir/libpath47.dylib`
             @osx_only `install_name_tool -change libpath47.dylib @rpath/libpath47.dylib $lib_dir/libpath47julia.dylib`
             @osx_only `install_name_tool -add_rpath $lib_dir $lib_dir/libpath47julia.dylib`
+        end
+        # Clean-up only.
+        @build_steps begin
+            ChangeDirectory(src_dir)
+            `rm -rf pathlib`
+            `rm -rf PathJulia`
+            `mv pathlib-$pathlib_v pathlib`
+            `mv PathJulia-$pathjulia_v PathJulia`
         end
     end), libpath47julia, os = :Darwin)
 
@@ -56,14 +66,12 @@ provides(BuildProcess,
         CreateDirectory(src_dir, true)
         @build_steps begin
             ChangeDirectory(src_dir)
-            FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip",
-                            joinpath(dl_dir, "pathlib.zip"))
+            FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip", joinpath(dl_dir, "pathlib.zip"))
             FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_so64)
             # `cp -i $libpath47_so64 $lib_dir`
         end
         @build_steps begin
-            FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.zip",
-                            joinpath(dl_dir, "PathJulia.zip"))
+            FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.zip", joinpath(dl_dir, "PathJulia.zip"))
             FileUnpacker(joinpath(dl_dir, "PathJulia.zip"), src_dir, path47julia_c)
             # `cp -i $libpath47julia_so64 $lib_dir`
         end
@@ -76,12 +84,12 @@ provides(BuildProcess,
         end
         @build_steps begin
             ChangeDirectory(joinpath(src_dir, "pathlib", "lib"))
-            `cp -i linux$bit/libpath47.so ../../../usr/lib/`
+            `cp -i linux$bit/libpath47.so $lib_dir`
         end
         @build_steps begin
             ChangeDirectory(joinpath(src_dir, "PathJulia", "src"))
             `make linux$bit`
-            `cp -i ../lib/linux$bit/libpath47julia.so ../../../usr/lib/`
+            `cp -i ../lib/linux$bit/libpath47julia.so $lib_dir`
         end
     end), libpath47julia, os = :Linux)
 
