f09848c75ceeb2e1828f279d3ee99b71ae9fcf94
Changhyun Kwon <chkwon@gmail.com>
2016-05-04 10:51:16 -0400

    windows build.jl
    win32: test passed, but cannot run multiple times
    
    win64; test failed, after solving all the problem. when the error
    message is ignored, it's fine.
    
    Seems like there is a memory-management issue.

--- deps/build.jl
+++ deps/build.jl
@@ -20,7 +20,8 @@ libgfortran_path = isempty(gfortran_osx) ? "" : gfortran_osx[1]
 
 # Verions of libraries
 pathlib_v = "a11966f36875748820583e41455800470c971171"
-pathjulia_v = "0.0.6"
+#pathjulia_v = "0.0.6"
+pathjulia_v = "windows"
 
 # The main dependency
 libpath47julia = library_dependency("libpath47julia")
@@ -28,8 +29,14 @@ libpath47julia = library_dependency("libpath47julia")
 libpath47_dylib = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "osx", "libpath47.dylib")
 libpath47julia_dylib = joinpath(src_dir, "PathJulia-$pathjulia_v", "lib", "osx", "libpath47julia.dylib")
 
-libpath47_so64 = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "linux$bit", "libpath47.so")
+libpath47_so = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "linux$bit", "libpath47.so")
 path47julia_c = joinpath(src_dir, "PathJulia-$pathjulia_v", "src", "pathjulia.c")
+
+libpath47_dll = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "win$bit", "path47.dll")
+libpath47_lib = joinpath(src_dir, "pathlib-$pathlib_v", "lib", "win$bit", "path47.lib")
+libpath47julia_dll = joinpath(src_dir, "PathJulia-$pathjulia_v", "lib", "win$bit", "libpath47julia.dll")
+libpath47julia_lib = joinpath(src_dir, "PathJulia-$pathjulia_v", "lib", "win$bit", "libpath47julia.lib")
+
 
 
 provides(BuildProcess,
@@ -55,13 +62,13 @@ provides(BuildProcess,
         @build_steps begin
             ChangeDirectory(src_dir)
             FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip", joinpath(dl_dir, "pathlib.zip"))
-            FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_so64)
-            # `cp -i $libpath47_so64 $lib_dir`
+            FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_so)
+            # `cp -i $libpath47_so $lib_dir`
         end
         @build_steps begin
             FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.zip", joinpath(dl_dir, "PathJulia.zip"))
             FileUnpacker(joinpath(dl_dir, "PathJulia.zip"), src_dir, path47julia_c)
-            # `cp -i $libpath47julia_so64 $lib_dir`
+            # `cp -i $libpath47julia_so $lib_dir`
         end
         @build_steps begin
             ChangeDirectory(src_dir)
@@ -77,4 +84,34 @@ provides(BuildProcess,
         end
     end), libpath47julia, os = :Linux)
 
+provides(BuildProcess,
+    (@build_steps begin
+        CreateDirectory(lib_dir, true)
+        CreateDirectory(src_dir, true)
+        @build_steps begin
+            ChangeDirectory(src_dir)
+            FileDownloader("https://github.com/ampl/pathlib/archive/$pathlib_v.zip", joinpath(dl_dir, "pathlib.zip"))
+            FileUnpacker(joinpath(dl_dir, "pathlib.zip"), src_dir, libpath47_dll)
+            `powershell -NoProfile -Command "Copy-Item -Path $libpath47_dll -Destination $lib_dir -force"`
+			`powershell -NoProfile -Command "Copy-Item -Path $libpath47_lib -Destination $lib_dir -force"`
+        end
+        @build_steps begin
+            FileDownloader("https://github.com/chkwon/PathJulia/archive/$pathjulia_v.zip", joinpath(dl_dir, "PathJulia.zip"))
+            FileUnpacker(joinpath(dl_dir, "PathJulia.zip"), src_dir, libpath47julia_dll)
+            `powershell -NoProfile -Command "Copy-Item -Path $libpath47julia_dll -Destination $lib_dir -force"`
+			`powershell -NoProfile -Command "Copy-Item -Path $libpath47julia_lib -Destination $lib_dir -force"`
+        end	
+        # @build_steps begin
+            # ChangeDirectory(src_dir)
+            # `powershell -Command "Remove-Item pathlib"`
+            # `powershell -Command "Remove-Item PathJulia"`
+            # `powershell -Command "mv pathlib-$pathlib_v pathlib"`
+            # `powershell -Command "mv PathJulia-$pathjulia_v PathJulia"`
+        # end
+    end), libpath47julia, os = :Windows)
+	
+@windows_only push!(BinDeps.defaults, BuildProcess)
+	
 @BinDeps.install Dict(:libpath47julia => :libpath47julia)
+
+@windows_only pop!(BinDeps.defaults)
