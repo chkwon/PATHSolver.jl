78a26d5a380dd29995d8f75f282c1b7b730a53c9
Changhyun Kwon <chkwon@usf.edu>
2016-08-20 22:01:00 -0400

    added element-wise function to the test

--- test/runtests.jl
+++ test/runtests.jl
@@ -57,3 +57,31 @@ status, z, f = solveMCP(myfunc, lb, ub)
 
 @test isapprox(z, [2.8, 0.0, 0.8, 1.2])
 @test status == :Solved
+
+
+
+println("-------------------------------------------------------")
+
+
+function elemfunc(x)
+    val = similar(x)
+    val[1] = -x[3]-x[4] + q[1]
+    val[2] = x[3] -2x[4] + q[2]
+    val[3] = x[1]-x[2]+2x[3]-2x[4] + q[3]
+    val[4] = x[1]+2x[2]-2x[3]+4x[4] + q[4]
+    return val
+end
+
+n = 4
+lb = zeros(n)
+ub = 100*ones(n)
+
+status, z, f = solveMCP(elemfunc, lb, ub)
+
+@show status
+@show z
+@show f
+
+
+@test isapprox(z, [2.8, 0.0, 0.8, 1.2])
+@test status == :Solved
