457801f07d16f177d4c19aa719a6ce11be694660
odow <o.dowson@gmail.com>
2024-04-10 12:02:19 +1200

    Switch to using Artifacts.toml for binaries

--- src/PATHSolver.jl
+++ src/PATHSolver.jl
@@ -5,85 +5,30 @@
 
 module PATHSolver
 
-import DataDeps
+import LazyArtifacts
 import MathOptInterface as MOI
 import SparseArrays
 
-function __init__()
-    platform = if Sys.iswindows()
-        "windows"
+function _get_artifact_path(filename)
+    root = LazyArtifacts.artifact"PATHSolver"
+    if Sys.iswindows()
+        return joinpath(root, "x86_64-w64-mingw32", "$filename.dll")
     elseif Sys.isapple()
-        Sys.ARCH == :aarch64 ? "silicon" : "macos"
+        return joinpath(root, "$(Sys.ARCH)-apple-darwin", "$filename.dylib")
     elseif Sys.islinux()
-        "linux"
+        return joinpath(root, "x86_64-linux-gnu", "$filename.so")
     else
         error("Unsupported platform.")
     end
-    libpath = Dict(
-        "windows" => (
-            "path50.dll",
-            "e227d19109f56628fccfdfedd7ecbdfd1667a3c975dd1d16a160a69d374d5474",
-        ),
-        "macos" => (
-            "libpath50.dylib",
-            "8787de93d21f49a46146ebe2ef5844d1c20a80f934a85f60164f9ddc670412f8",
-        ),
-        "silicon" => (
-            "libpath50.silicon.dylib",
-            "4e667615180565062013ab50a3968bbeddf3e510a9cdbfc27aa685152742b637",
-        ),
-        "linux" => (
-            "libpath50.so",
-            "8c36baaea0952729788ec8d964253305b04b0289a1d74ca5606862c9ddb8f2fd",
-        ),
-    )
-    libpath_filename, libpath_sha256 = libpath[platform]
-    DataDeps.register(
-        DataDeps.DataDep(
-            "libpath50",
-            "The libpath50 binary from http://pages.cs.wisc.edu/~ferris",
-            "http://pages.cs.wisc.edu/~ferris/path/julia/$(libpath_filename)",
-            libpath_sha256,
-        ),
-    )
-    lusol = Dict(
-        "windows" => (
-            "lusol.dll",
-            "2e1f0ed17914ddcf1b833898731ff4b85afab0cf914e0707dcff9e4e995cebd8",
-        ),
-        "macos" => (
-            "liblusol.dylib",
-            "52d631fd3d753581c62d5b4b636e9cb3f8cc822738fe34c6879443d5b5092f12",
-        ),
-        "silicon" => (
-            "liblusol.silicon.dylib",
-            "24113f8730cdaa567c70eee36209720e37ade6080dc62bfd5ee873da0357b88d",
-        ),
-        "linux" => (
-            "liblusol.so",
-            "ca87167853cdac9d4697a51a588d13ed9a7c093219743efa1d250cb62ac3dcb7",
-        ),
-    )
-    liblusol_filename, liblusol_sha256 = lusol[platform]
-    DataDeps.register(
-        DataDeps.DataDep(
-            "liblusol",
-            "The lusol binary for use with PATH from http://pages.cs.wisc.edu/~ferris",
-            "http://pages.cs.wisc.edu/~ferris/path/julia/$(liblusol_filename)",
-            liblusol_sha256,
-        ),
-    )
+end
+
+function __init__()
     if haskey(ENV, "PATH_JL_LOCATION")
         global PATH_SOLVER = ENV["PATH_JL_LOCATION"]
         global LUSOL_LIBRARY_PATH = ""
     else
-        current = get(ENV, "DATADEPS_ALWAYS_ACCEPT", "false")
-        ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
-        global PATH_SOLVER =
-            joinpath(DataDeps.datadep"libpath50", libpath_filename)
-        global LUSOL_LIBRARY_PATH =
-            joinpath(DataDeps.datadep"liblusol", liblusol_filename)
-        ENV["DATADEPS_ALWAYS_ACCEPT"] = current
+        global PATH_SOLVER = _get_artifact_path("libpath")
+        global LUSOL_LIBRARY_PATH = _get_artifact_path("liblusol")
     end
     return
 end
