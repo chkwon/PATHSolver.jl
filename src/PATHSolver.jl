08c0f4bbb4baac4ce1f8619c1eefbcdd7f15597a
Oscar Dowson <odow@users.noreply.github.com>
2025-09-23 15:14:11 +1200

    Change artifact permissions for Windows DLLs

--- src/PATHSolver.jl
+++ src/PATHSolver.jl
@@ -12,7 +12,9 @@ import SparseArrays
 function _get_artifact_path(file)
     root = LazyArtifacts.artifact"PATHSolver"
     if Sys.iswindows()  # There's a permission error with the artifact
-        chmod(root, 0o755; recursive = true)
+        for f in filter(endswith(".dll"), readdir(root; join = true))
+            chmod(f, 0o755)
+        end
     end
     triplet = join(split(Base.BUILD_TRIPLET, "-")[1:3], "-")
     ext = ifelse(Sys.iswindows(), "dll", ifelse(Sys.isapple(), "dylib", "so"))
